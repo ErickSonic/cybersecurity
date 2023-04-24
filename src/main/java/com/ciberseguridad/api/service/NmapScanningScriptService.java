eb3e532039d5f74ab790c7530847cdc701f2fbdf
Erick Isaí Maya Vega <Erick Maya>
2023-04-24 14:04:33 -0600

    Bash

--- src/main/java/com/ciberseguridad/api/service/NmapScanningScriptService.java
+++ src/main/java/com/ciberseguridad/api/service/NmapScanningScriptService.java
@@ -42,8 +42,8 @@ public class NmapScanningScriptService {
 	}
 	
 	public HashMap<String,List<HashMap<String,String>>> processVulnerabilitiesScript(String script) throws IOException{
-		processBuilder.command("cmd.exe", "/c", script);
-	    //processBuilder.command("bash", "-c", script);
+		//processBuilder.command("cmd.exe", "/c", script);
+	    processBuilder.command("bash", "-c", script);
 	
 	    try {
 	
