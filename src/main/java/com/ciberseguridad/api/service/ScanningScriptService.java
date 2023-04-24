76d47caac7fe43774ac5cbd9e95bead786f89849
Erick Isaí Maya Vega <Erick Maya>
2023-04-24 14:05:51 -0600

    Bash

--- src/main/java/com/ciberseguridad/api/service/ScanningScriptService.java
+++ src/main/java/com/ciberseguridad/api/service/ScanningScriptService.java
@@ -12,8 +12,8 @@ public class ScanningScriptService {
 	public String processScript(String script) throws IOException{
         ProcessBuilder processBuilder = new ProcessBuilder();
         
-	    processBuilder.command("cmd.exe", "/c", script);
-	    //processBuilder.command("bash", "-c", script);
+	    //processBuilder.command("cmd.exe", "/c", script);
+	    processBuilder.command("bash", "-c", script);
 	
 	    try {
 	
